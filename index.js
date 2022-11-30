17cff1675814de702024ff5562bde13ec497a414
Musyoki_Wambua <wjosephmusyoki@gmail.com>
2022-12-01 00:10:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+
+
+const totalBatteries = batteryBatches.reduce((total, amount) => {
+    total + amount;{
+        return total + amount 
+
+    }
+})
+
+
